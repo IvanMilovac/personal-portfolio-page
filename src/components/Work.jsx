be819649b91fc84de27e20baf618beb38dc60b29
imilovac <Ivanmilovac1993@hotmail.com>
2022-01-23 22:30:55 +0100

    Added 2 project to portfolio site

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,18 +1,48 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Card from "./shared/Card.jsx";
+
+import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
+
+import ECom from "../assets/ecom-snip.PNG";
+import CRMSnip from "../assets/CRM-snip.PNG";
+import MUI from "../assets/mui.png";
+import TS from "../assets/typescript.png";
+import reactIcon from "../assets/react.png";
+import sass from "../assets/sass.png";
+import Webpack from "../assets/webpack.png";
+import Babel from "../assets/babel.png";
 
 import "./Work.scss";
 
 const Work = () => {
   return (
     <div className="work-container">
-      <h2>
-        You can check out my portfolio on my{"   "}
-        <a href="https://github.com/IvanMilovac" target="_blank">
-          GitHub
-        </a>
-        {"   "}
-        account
-      </h2>
+      <Card
+        title={<h3 className="card-title">EComm App</h3>}
+        gitLink="https://github.com/IvanMilovac/react-ts-ecommerce"
+        liveLink="https://e-com-im.web.app/"
+        image={ECom}
+        techStack={[reactIcon, MUI, TS]}
+      />
+      <Card
+        title={<h3 className="card-title">CRM App</h3>}
+        gitLink="https://github.com/IvanMilovac/CRM-react"
+        liveLink="https://crm-project-im.web.app/login"
+        image={CRMSnip}
+        techStack={[reactIcon, sass, Webpack, Babel]}
+      />
+      <Card
+        title={
+          <h3 className="card-title">
+            <a href="https://github.com/IvanMilovac" target="_blank">
+              Other project are on my Github account{" "}
+              <FontAwesomeIcon icon={faLongArrowAltRight} />
+            </a>
+          </h3>
+        }
+      />
     </div>
   );
 };
